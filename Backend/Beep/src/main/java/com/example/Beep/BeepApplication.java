5f71a559af6ad9ce376f83b0ac751df15fe5b619
tlsrbwls999 <tlsrbwls999@naver.com>
2022-10-20 11:36:19 +0900

    :sparkles: 백엔드 셋팅 추가 Gradle

---
+++ Backend/Beep/src/main/java/com/example/Beep/BeepApplication.java
@@ -0,0 +1,13 @@
+package com.example.Beep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeepApplication.class, args);
+	}
+
+}
