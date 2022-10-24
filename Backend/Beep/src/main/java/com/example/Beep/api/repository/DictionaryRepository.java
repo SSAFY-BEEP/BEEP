5ab33a5d382de9b3751c5d30a3230348e0e96857
tlsrbwls999 <tlsrbwls999@naver.com>
2022-10-24 17:35:53 +0900

    :art: BE_"디렉토리 생성"

---
+++ Backend/Beep/src/main/java/com/example/Beep/api/repository/DictionaryRepository.java
@@ -0,0 +1,6 @@
+package com.example.Beep.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DictionaryRepository extends JpaRepository<DictionaryRepository,Long> {
+}
