ad7f0991b7206593f0bc57b581ca6434eea4560b
tlsrbwls999 <tlsrbwls999@naver.com>
2022-10-27 10:58:36 +0900

    :sparkles: BE_"사전 및 퀴즈 API 생성(properties 수정함)"

--- Backend/Beep/src/main/java/com/example/Beep/api/service/DictionaryService.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/service/DictionaryService.java
@@ -1,4 +1,14 @@
 package com.example.Beep.api.service;
 
+import com.example.Beep.api.domain.dto.DictionaryResponseDto;
+
+import java.util.List;
+
 public interface DictionaryService {
+
+    List<DictionaryResponseDto> FindWord(String word);
+
+    List<DictionaryResponseDto> FindValue(String value);
+
+    List<DictionaryResponseDto>FindRandom();
 }
--- Backend/Beep/src/main/java/com/example/Beep/api/service/DictionaryServiceImpl.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/service/DictionaryServiceImpl.java
@@ -1,4 +1,54 @@
 package com.example.Beep.api.service;
 
-public class DictionaryServiceImpl {
+import com.example.Beep.api.domain.dto.DictionaryResponseDto;
+import com.example.Beep.api.repository.DictionaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DictionaryServiceImpl implements DictionaryService{
+
+    @Autowired
+    DictionaryRepository dictionaryRepository;
+
+    public List<DictionaryResponseDto> FindWord(String word) {
+        List<DictionaryResponseDto>dictionaryResponseDtoList=dictionaryRepository.findByWordContaining(word)
+                .stream()
+                .map(Dictionary -> DictionaryResponseDto.builder()
+                        .word(Dictionary.getWord())
+                        .value(Dictionary.getValue())
+                        .build()).collect(Collectors.toList());
+
+        return dictionaryResponseDtoList;
+    }
+
+    public List<DictionaryResponseDto> FindValue(String value) {
+        List<DictionaryResponseDto>dictionaryResponseDtoList=dictionaryRepository.findByValueContaining(value)
+                .stream()
+                .map(Dictionary -> DictionaryResponseDto.builder()
+                        .word(Dictionary.getWord())
+                        .value(Dictionary.getValue())
+                        .build()).collect(Collectors.toList());
+
+        return dictionaryResponseDtoList;
+    }
+
+    @Override
+    public List<DictionaryResponseDto> FindRandom() {
+        List<DictionaryResponseDto>dictionaryResponseDtoList=dictionaryRepository.findRandom()
+                .stream()
+                .map(Dictionary -> DictionaryResponseDto.builder()
+                        .word(Dictionary.getWord())
+                        .value(Dictionary.getValue())
+                        .build()).collect(Collectors.toList());
+
+        return dictionaryResponseDtoList;
+    }
 }
