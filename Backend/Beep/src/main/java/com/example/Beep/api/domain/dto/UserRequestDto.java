8d88c24442a9953a515496f9aba84b9ee5babbcf
황준원 <miso97o@naver.com>
2022-10-28 15:31:50 +0900

    :sparkles: BE_스웨거 api 설명 작성

--- Backend/Beep/src/main/java/com/example/Beep/api/domain/dto/UserRequestDto.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/domain/dto/UserRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.Beep.api.domain.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,8 +9,11 @@ public class UserRequestDto {
     @Getter
     @Builder
     public static class SignUp {
+        @ApiModelProperty(example = "01012345678")
         String phoneNumber;
+        @ApiModelProperty(example = "1234asdf")
         String password;
+        @ApiModelProperty(example = "토큰")
         String fcmToken;
     }
 
