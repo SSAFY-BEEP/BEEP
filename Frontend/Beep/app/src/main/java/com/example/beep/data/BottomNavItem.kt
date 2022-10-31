2918119769f9c508ce7245da298df1b3f8bc5175
taeha <rlaxogk006@naver.com>
2022-10-31 15:37:31 +0900

    :wrench: FE_Compose 구조 설정, Navigation 추가
    1. Topbar, BottomNavbar를 추가해 기본적인 틀을 잡았습니다.
    2. Home, Message, MyPage를 위한 Navigation을 추가했습니다.
    - 아직 아이콘, 테마 등은 완전히 변경되지 않아 추가 수정이 필요합니다.

---
+++ Frontend/Beep/app/src/main/java/com/example/beep/data/BottomNavItem.kt
@@ -0,0 +1,8 @@
+package com.example.beep.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(val name: String,
+                         val route: String,
+                         val icon: ImageVector,
+                         val badgeCount: Int = 0)
