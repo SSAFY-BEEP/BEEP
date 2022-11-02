6840e5dc547aa187a475853aa8de4e73874443d2
tlsrbwls999 <tlsrbwls999@naver.com>
2022-11-02 14:23:44 +0900

    :sparkles: BE_"메시지 보관 API 스웨거 설정"

--- Backend/Beep/src/main/java/com/example/Beep/api/controller/MessageController.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/controller/MessageController.java
@@ -5,6 +5,7 @@ import com.example.Beep.api.domain.dto.MessageResponseDto;
 import com.example.Beep.api.domain.entity.Message24;
 import com.example.Beep.api.service.MessageService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,36 +21,42 @@ public class MessageController {
 
     private final MessageService messageService;
 
+    @ApiOperation(value = "내가 보낸 메시지 찾기", notes = "내가 보낸 메시지 찾기")
     @GetMapping("/findsend/{id}")
     public ResponseEntity<?>findSendMessage(@PathVariable("id") Long id){
         List<MessageResponseDto>messageResponseDtoList=messageService.findSendMessage(id);
         return new ResponseEntity<List<MessageResponseDto>>(messageResponseDtoList, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "내가 받은 메시지 찾기", notes = "내가 받은 메시지 찾기")
     @GetMapping("/findrecieve/{id}")
     public ResponseEntity<?>findReceiceMessage(@PathVariable("id") Long id){
         List<MessageResponseDto>messageResponseDtoList=messageService.findReceiveMessage(id);
         return new ResponseEntity<List<MessageResponseDto>>(messageResponseDtoList, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "내가 보낸 메시지 저장", notes = "내가 보낸 메시지 저장")
     @PostMapping("/save/send")
     public ResponseEntity<?>saveSendMessage(@RequestBody MessageRequestDto.persistMessage persistMessage){
         messageService.saveSendMessage(persistMessage);
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
+    @ApiOperation(value = "내가 받은 메시지 저장", notes = "내가 받은 메시지 저장")
     @PostMapping("/save/receive")
     public ResponseEntity<?>saveReceiveMessage(@RequestBody MessageRequestDto.persistMessage persistMessage){
         messageService.saveReceiveMessage(persistMessage);
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @ApiOperation(value = "보관함에서 삭제", notes = "보관함에서 삭제")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?>deleteMessage(@PathVariable("id") Long id){
         messageService.deleteMessage(id);
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
+    @ApiOperation(value = "태그 추가", notes = "태그 추가")
     @PatchMapping("/tag")
     public ResponseEntity<?>updateTag(@RequestBody MessageRequestDto.updateTag updateTag){
         messageService.updateTag(updateTag);
