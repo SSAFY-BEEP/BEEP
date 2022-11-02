cafa78724e30df69587ab91d5d2af8ef30ab9943
taeha <rlaxogk006@naver.com>
2022-11-02 13:21:16 +0900

    :art: Retrofit API 테스트 코드 추가
    1. Retrofit API 테스트 요청에 필요한 코드들을 추가했습니다.
    - DTO, Response 클래스 추가
    - Api interface에 관련 메서드 추가 및 관련 파일 모두 수정
    - https://reqres.in/의 SINGLE USER 요청 참고

--- Frontend/Beep/app/src/main/java/com/example/beep/data/api/RetrofitApi.kt
+++ Frontend/Beep/app/src/main/java/com/example/beep/data/api/RetrofitApi.kt
@@ -1,6 +1,7 @@
 package com.example.beep.data.api
 
 import com.example.beep.data.dto.DataModel
+import com.example.beep.data.dto.RetrofitTestGetResponse
 import com.example.beep.data.dto.RetrofitTestResponse
 import retrofit2.Call
 import retrofit2.http.Body
@@ -13,4 +14,7 @@ interface RetrofitApi {
 
     @POST("users")
     suspend fun postData(@Body dataModel: DataModel?): RetrofitTestResponse
+
+    @GET("users/2")
+    suspend fun getData(): RetrofitTestGetResponse
 }
