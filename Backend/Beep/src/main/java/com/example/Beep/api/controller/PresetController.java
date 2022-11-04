c40799646c269ac3045425c647002cfa86011a3a
박예진 <80744051+yaejin-park@users.noreply.github.com>
2022-11-04 17:20:50 +0900

    :recycle: 전체컨트롤러 스프링시큐리티 및 스웨거 설정
    - 컨트롤러 어노테이션 적용
    : CrossOrigin("*")
    : RequiredArgsController 사용하고 Autowired 대신 의존성 주입
    - 스프링 시큐리티 권한 설정
    - 스웨거 컨트롤러명

--- Backend/Beep/src/main/java/com/example/Beep/api/controller/PresetController.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/controller/PresetController.java
@@ -7,35 +7,41 @@ import com.example.Beep.api.domain.entity.Preset;
 import com.example.Beep.api.service.PresetServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(value = "5. 프리셋 설정",tags={"5. 프리셋 설정 API"})
+@Api(value = "7. 프리셋",tags={"7. 프리셋"})
+@RequiredArgsConstructor
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/preset")
 public class PresetController {
 
-    @Autowired
-    PresetServiceImpl presetService;
+    private final PresetServiceImpl presetService;
 
     @ApiOperation(value = "프리셋 설정 및 업데이트", notes = "프리셋 설정 및 업데이트")
     @PostMapping("/save")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public void PresetSave(@RequestBody PresetRequestDto presetRequestDto){
         presetService.PresetSave(presetRequestDto);
     }
 
     @ApiOperation(value = "프리셋 삭제", notes = "프리셋 삭제")
     @DeleteMapping("/delete/{pid}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public void PresetDelete(@PathVariable("pid") Long pid){
         presetService.PresetDelete(pid);
     }
 
     @ApiOperation(value = "유저 프리셋 찾기", notes = "유저 프리셋 찾기")
     @GetMapping("/find/{uid}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<?> PresetFind(@PathVariable("uid") Long uid){
         return new ResponseEntity<List<PresetResponseDto>>(presetService.PresetFind(uid),HttpStatus.OK);
     }
--- Backend/Beep/src/main/java/com/example/Beep/api/controller/SMSCode24Controller.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/controller/SMSCode24Controller.java
@@ -7,11 +7,13 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@Api(value = "9. 인증번호 발송(테스트용)", tags={"9. 인증번호 발송(테스트용)"})
+@Api(value = "10. SMS 인증번호", tags={"10. SMS 인증번호"})
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 @RequestMapping("/smscode24")
 public class SMSCode24Controller {
     private final SMSCode24Service smsCode24Service;
@@ -20,6 +22,7 @@ public class SMSCode24Controller {
 
     @ApiOperation(value = "문자확인코드 레디스에 저장", notes = "이미 존재하면 데이터 수정, 아니면 데이터 입력(3분 유효기간)")
     @PostMapping
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> sendCode(@RequestParam String phoneNumber, @RequestParam String code) {
         smsCode24Service.saveCode(phoneNumber, code);
         return new ResponseEntity<>("성공", HttpStatus.OK);
@@ -27,6 +30,7 @@ public class SMSCode24Controller {
 
     @ApiOperation(value = "문자확인코드 인증", notes = "핸드폰번호와 확인코드가 일치하면 true, 아니면 false")
     @GetMapping("/authorization")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> isCorrect(@RequestParam String phoneNumber, @RequestParam String code) {
         return new ResponseEntity<>(smsCode24Service.isCorrect(phoneNumber,code), HttpStatus.OK);
     }
