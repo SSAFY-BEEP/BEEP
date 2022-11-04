6818998b3f4a405e196a6f3bdcb6ea20c9f79466
박예진 <80744051+yaejin-park@users.noreply.github.com>
2022-11-04 11:03:43 +0900

    :recycle: 메세지 타입 enum 수정
    - SAVE를 BLOCK으로 잘못 작성한 것 수정
    - ENUM에 name이 필요없어서 num만 두고 삭제

--- Backend/Beep/src/main/java/com/example/Beep/api/domain/enums/MessageType.java
+++ Backend/Beep/src/main/java/com/example/Beep/api/domain/enums/MessageType.java
@@ -7,9 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MessageType {
-    SAVE("SAVE", 1),
-    BLOCK("BLOCK", 2);
+    SAVE(1),
+    BLOCK(2);
 
-    private final String Name;
     private final Integer Num;
 }
