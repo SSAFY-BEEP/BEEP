3e96b27505e05323dae5619f2339a655325b3133
Hyeon Yeol Kim <gusduf612@gmail.com>
2022-11-10 10:26:59 +0900

    :bug: FE_ 구조 변경에 따른 파일 수정
    프리셋 수정
    메세지 프리셋 화면 수정

--- Frontend/Beep/app/src/main/java/com/example/beep/data/repository/PresetRepository.kt
+++ Frontend/Beep/app/src/main/java/com/example/beep/data/repository/PresetRepository.kt
@@ -1,7 +1,7 @@
 package com.example.beep.data.repository
 
-import com.example.beep.data.dto.mypage.PresetResponse
 import com.example.beep.data.datasource.PresetDataSource
+import com.example.beep.data.dto.mypage.PresetResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.Response
@@ -14,8 +14,8 @@ class PresetRepository @Inject constructor(private val presetDataSource: PresetD
     fun getUserPresetByToken(): Flow<Response<List<PresetResponse>>> =
         flow { presetDataSource.getUserPresetByToken().collect { emit(it) } }
 
-    fun updatePreset(uid : Long, number : Int, part : Int, content : String): Flow<Response<String>> =
-        flow { presetDataSource.updatePreset(uid, number, part, content).collect { emit(it) }}
+    fun updatePreset(uid: Long, number: Int, part: Int, content: String): Flow<Response<String>> =
+        flow { presetDataSource.updatePreset(uid, number, part, content).collect { emit(it) } }
 
     fun deletePreset(pid: Long): Flow<Response<String>> =
         flow { presetDataSource.deletePreset(pid).collect { emit(it) } }
