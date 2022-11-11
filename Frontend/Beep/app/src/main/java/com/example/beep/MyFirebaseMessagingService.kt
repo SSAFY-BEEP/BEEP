ce7c2970a06bcf68734884ca8837a8f5e4d6a5b5
황준원 <miso97o@naver.com>
2022-11-11 09:35:17 +0900

    :sparkles: FE_fcm 수신 알림 코드 추가

--- Frontend/Beep/app/src/main/java/com/example/beep/MyFirebaseMessagingService.kt
+++ Frontend/Beep/app/src/main/java/com/example/beep/MyFirebaseMessagingService.kt
@@ -1,7 +1,15 @@
 package com.example.beep
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -25,6 +33,9 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d("Firebase", "Message data payload: ${remoteMessage.data}")
+            sendNotification(remoteMessage)
+        } else {
+            Log.e("Firebase", "data가 비어있습니다. 메시지를 수신하지 못했습니다.")
         }
 
         // Check if message contains a notification payload.
@@ -34,5 +45,46 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    // 받은 알림을 기기에 표시하는 메서드
+    private fun sendNotification(remoteMessage: RemoteMessage)
+    {
+        // RequestCode, Id를 고유값으로 지정하여 알림이 개별 표시
+        val uniId: Int = (System.currentTimeMillis() / 7).toInt()
+
+        // 일회용 PendingIntent : Intent 의 실행 권한을 외부의 어플리케이션에게 위임
+        val intent = Intent(this, MainActivity::class.java)
+        //각 key, value 추가
+        for(key in remoteMessage.data.keys){
+            intent.putExtra(key, remoteMessage.data.getValue(key))
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남김(A-B-C-D-B => A-B)
+        val pendingIntent = PendingIntent.getActivity(this, uniId, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        // 알림 채널 이름
+        val channelId = "my_channel"
+        // 알림 소리
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        // 알림에 대한 UI 정보, 작업
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
+            .setContentTitle(remoteMessage.data["header"].toString()) // 제목
+            .setContentText(remoteMessage.data["content"].toString()) // 메시지 내용
+            .setAutoCancel(true) // 알람클릭시 삭제여부
+            .setSound(soundUri)  // 알림 소리
+            .setContentIntent(pendingIntent) // 알림 실행 시 Intent
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // 오레오 버전 이후에는 채널이 필요
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // 알림 생성
+        notificationManager.notify(uniId, notificationBuilder.build())
     }
 }
