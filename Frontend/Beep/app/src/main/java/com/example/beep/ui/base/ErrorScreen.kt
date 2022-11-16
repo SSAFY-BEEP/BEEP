3b86f02be41f86c200b3655d8273f48549fa1fe4
Hyeon Yeol Kim <gusduf612@gmail.com>
2022-11-16 16:01:55 +0900

    :lipstick: FE_마이페이지 UI 변경

--- Frontend/Beep/app/src/main/java/com/example/beep/ui/base/ErrorScreen.kt
+++ Frontend/Beep/app/src/main/java/com/example/beep/ui/base/ErrorScreen.kt
@@ -1,9 +1,20 @@
 package com.example.beep.ui.base
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ErrorScreen() {
-    Text(text = "에러가 발생했습니다.")
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "에러가 발생했습니다.")
+    }
+
 }
--- Frontend/Beep/app/src/main/java/com/example/beep/ui/base/LoadingScreen.kt
+++ Frontend/Beep/app/src/main/java/com/example/beep/ui/base/LoadingScreen.kt
@@ -1,10 +1,19 @@
 package com.example.beep.ui.base
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun LoadingScreen(modifier: Modifier = Modifier) {
-    Text(text = "로딩중")
+fun LoadingScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "로딩중")
+    }
+
 }
